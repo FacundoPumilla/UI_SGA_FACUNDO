0299330f47afe9f86f024f82f4b969df5b134abd
matias <matiasluduenia@gmail.com>
2022-07-03 18:14:24 -0300

    Persona list tabla

--- ui/src/app/core/components/persona/persona-list/persona-list.component.ts
+++ ui/src/app/core/components/persona/persona-list/persona-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PersonaService } from 'src/app/core/services/persona.service';
+import { Persona } from 'src/app/model/persona';
 
 @Component({
   selector: 'app-persona-list',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./persona-list.component.css']
 })
 export class PersonaListComponent implements OnInit {
+  personaList:Persona[];
+  displayedColumns: string[] = ['id','nombre','apellido', 'dni'];
 
-  constructor() { }
+  constructor(
+    private personaServices: PersonaService
+  ) { }
 
   ngOnInit(): void {
+    this.GetPersonaList()
+  }
+  GetPersonaList():void{
+    this.personaServices.findAll().subscribe((response:any)=>{
+      this.personaList= response as Persona[];
+      console.log(this.personaList);
+    })
   }
 
 }
